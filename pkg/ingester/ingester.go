d0a0bad3a272a06eba483c5da966eb72d515f5ef
Christian Haudum <christian.haudum@gmail.com>
2023-07-20 15:54:14 +0200

    Smooth out chunk flush operations
    In order to prevent spikes in chunk flush operations every flush check
    period, we smooth out the flush operations over the period of the
    interval.
    
    Signed-off-by: Christian Haudum <christian.haudum@gmail.com>

--- pkg/ingester/ingester.go
+++ pkg/ingester/ingester.go
@@ -24,6 +24,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/model/labels"
+	"golang.org/x/time/rate"
 	"google.golang.org/grpc/health/grpc_health_v1"
 
 	"github.com/grafana/loki/pkg/analytics"
@@ -222,6 +223,9 @@ type Ingester struct {
 	flushQueues     []*util.PriorityQueue
 	flushQueuesDone sync.WaitGroup
 
+	// Spread out calls to the chunk store over the flush period
+	flushRateLimiter *rate.Limiter
+
 	limiter *Limiter
 
 	// Denotes whether the ingester should flush on shutdown.
@@ -268,6 +272,7 @@ func New(cfg Config, clientConfig client.Config, store ChunkStore, limits Limits
 		periodicConfigs:       store.GetSchemaConfigs(),
 		loopQuit:              make(chan struct{}),
 		flushQueues:           make([]*util.PriorityQueue, cfg.ConcurrentFlushes),
+		flushRateLimiter:      rate.NewLimiter(rate.Inf, 1),
 		tailersQuit:           make(chan struct{}),
 		metrics:               metrics,
 		flushOnShutdownSwitch: &OnceSwitch{},
